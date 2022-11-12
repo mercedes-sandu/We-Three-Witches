135c4bad012979e9b244c277f0f6cec63acf63d1
mercedes sandu <mxrcedes@gmail.com>
2022-11-12 12:50:58 -0600

    comments

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,27 +5,48 @@ using Pathfinding;
 
 public class EnemyAI : MonoBehaviour
 {
+    /// <summary>
+    /// The transform of the target the enemy will follow.
+    /// </summary>
     public Transform target;
 
+    /// <summary>
+    /// The speed of the enemy.
+    /// </summary>
     public float speed = 7f;
     
+    /// <summary>
+    /// The distance at which the enemy will stop following the target.
+    /// </summary>
     public float nextWaypointDistance = 3f;
     
     /// <summary>
-    /// 
+    /// True if the enemy is a flying type, false otherwise.
     /// </summary>
     [SerializeField] private bool isFlying = false;
 
+    /// <summary>
+    /// The path the enemy will follow.
+    /// </summary>
     private Path _path;
 
+    /// <summary>
+    /// The enemy's current way point.
+    /// </summary>
     private int _currentWayPoint = 0;
     
+    /// <summary>
+    /// True if the enemy has reached the end of the path, false otherwise.
+    /// </summary>
     private bool _reachedEndOfPath = false;
 
+    /// <summary>
+    /// The seeker component of the enemy.
+    /// </summary>
     private Seeker _seeker;
 
     /// <summary>
-    /// 
+    /// The Rigidbody2D component of the enemy.
     /// </summary>
     private Rigidbody2D _rb;
     
@@ -37,7 +58,9 @@ public class EnemyAI : MonoBehaviour
         GameEvent.OnPlayerCreate += CreatePlayer;
     }
     
-    // Start is called before the first frame update
+    /// <summary>
+    /// Initializes components and calls for the path to be updated.
+    /// </summary>
     void Start()
     {
         _seeker = GetComponent<Seeker>();
@@ -48,6 +71,9 @@ public class EnemyAI : MonoBehaviour
         InvokeRepeating(nameof(UpdatePath), 0f, 0.5f);
     }
 
+    /// <summary>
+    /// Updates the path to the target.
+    /// </summary>
     private void UpdatePath()
     {
         if (_seeker.IsDone())
@@ -57,18 +83,18 @@ public class EnemyAI : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Initializes the target to be the player.
     /// </summary>
-    /// <param name="player"></param>
+    /// <param name="player">The player.</param>
     private void CreatePlayer(GameObject player)
     {
         target = player.transform;
     }
     
     /// <summary>
-    /// 
+    /// Called when the path has been completed.
     /// </summary>
-    /// <param name="p"></param>
+    /// <param name="p">The path.</param>
     private void OnPathComplete(Path p)
     {
         if (!p.error)
@@ -79,7 +105,7 @@ public class EnemyAI : MonoBehaviour
     }
 
     /// <summary>
-    /// 
+    /// Updates the enemy's position and movement.
     /// </summary>
     void FixedUpdate()
     {
@@ -91,6 +117,8 @@ public class EnemyAI : MonoBehaviour
         if (_currentWayPoint >= _path.vectorPath.Count)
         {
             _reachedEndOfPath = true;
+            // todo: attack enemy
+            Debug.Log("attack enemy");
             return;
         }
         else
